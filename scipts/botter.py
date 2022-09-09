9b13742348c09064b2326e5d39beeb185c5e6b0c
natrix_dev <discordlenko@gmail.com>
2022-09-09 18:17:59 +0200

    Update botter.py

--- scipts/botter.py
+++ scipts/botter.py
@@ -1,1 +1,7 @@
+import discord; import request; import threading; import os; import time ; import clear; import console
+
+assets="https://github.com/natrixdev/discord-mass-friend-add/tree/main/assets"
+config="https://github.com/natrixdev/discord-mass-friend-add/tree/main/config"
+run_first="https://github.com/natrixdev/discord-mass-friend-add/blob/main/RUN%20FIRST.bat"
+run_second="https://github.com/natrixdev/discord-mass-friend-add/blob/main/RUN%20SECOND.bat"
 
