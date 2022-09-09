36c877b30c68c5bfc972ed496f362ee7d1e8cdd0
natrix_dev <discordlenko@gmail.com>
2022-09-09 18:46:48 +0200

    Update botter.py

--- scipts/botter.py
+++ scipts/botter.py
@@ -1,7 +1,53 @@
 import discord; import request; import threading; import os; import time ; import clear; import console
 
+global = "https://github.com/natrixdev/discord-mass-friend-add"
+
 assets="https://github.com/natrixdev/discord-mass-friend-add/tree/main/assets"
 config="https://github.com/natrixdev/discord-mass-friend-add/tree/main/config"
 run_first="https://github.com/natrixdev/discord-mass-friend-add/blob/main/RUN%20FIRST.bat"
 run_second="https://github.com/natrixdev/discord-mass-friend-add/blob/main/RUN%20SECOND.bat"
+scripts="https://github.com/natrixdev/discord-mass-friend-add/tree/main/scipts"
 
+fonts = "https://fonts.google.com"
+spefont = "https://fonts.google.com/specimen/Roboto"
+
+helppage = "https://github.com/natrixdev/discord-mass-friend-add/blob/main/README.md"
+issuespage ="https://github.com/natrixdev/discord-mass-friend-add/issues"
+
+cloner = "gh repo clone natrixdev/discord-mass-friend-add"
+
+write_sounds = "https://drive.google.com/file/d/1DlMCl9VSWD4c7NkekR8JgYPeAWymxCbx/view"
+
+path = [ ]
+
+files = [ ]
+
+discord = " " 
+
+for main in "discord-mass-friend-add/scipts/botter.py":
+  if run_first.clicked=True:
+    if run_second.clicked=True:
+      print('Bot is ready to run')
+  else if run_first.clicked=False:
+    print('Please install packages with "run_first" file.')
+    else if run_second.clicked=False:
+    print('Please install packages with "run_second" file.')
+    
+  else: 
+    print('There was an error during the modules installation. Please try again later')
+    
+  append()
+  
+  def __main__:
+    if main.complete()
+      else: 
+        goto main
+    else 
+   os.cmd('pip --version') 
+   os.newWindow("400x400px")
+   os.set("title Discord Friend Botter")
+   os.add{
+     newButton["input", "number of freinds", "max: 20"]
+     newButton["click", "send", "NaN" ]
+     newTitle["Discord Friend Botter"]
+    
